a14b84c47293661e5704512546ca27523a3f2508
DennisPierantozzi <s1093366@studenti.univpm.it>
2022-05-24 18:19:25 +0200

    fix bugs

---
+++ app/src/main/java/com/example/progettoprogrammazionemobile/HomeActivity.kt
@@ -0,0 +1,21 @@
+package com.example.progettoprogrammazionemobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        setSupportActionBar(findViewById(R.id.topAppBar))
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+}
