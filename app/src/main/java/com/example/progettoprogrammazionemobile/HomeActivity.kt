382ee6698c57557daa1e26ffb988941367394015
DennisPierantozzi <s1093366@studenti.univpm.it>
2022-06-08 23:53:01 +0200

    navigate controller bottom menu

--- app/src/main/java/com/example/progettoprogrammazionemobile/HomeActivity.kt
+++ app/src/main/java/com/example/progettoprogrammazionemobile/HomeActivity.kt
@@ -3,7 +3,10 @@ package com.example.progettoprogrammazionemobile
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toolbar
 import androidx.navigation.findNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
 
@@ -14,14 +17,29 @@ class HomeActivity : AppCompatActivity() {
 
 
         //val navController = this.findNavController(R.id.myNavHostFragment)
-        print("provaosva")
         setSupportActionBar(findViewById(R.id.topAppBar))
 
+
+        val mOnNavigationItemSelectedListener =
+            BottomNavigationView.OnNavigationItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.icon_user -> {
+                        val navController = this.findNavController(R.id.myNavHostFragment)
+                        navController.navigate(R.id.action_homeFragment_to_profilo)
+                        return@OnNavigationItemSelectedListener true
+                    }
+
+                }
+                false
+            }
+
+        val bottomAppBar = findViewById<BottomNavigationView>(R.id.bottomAppBar)
+        bottomAppBar.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 
-   override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.top_menu, menu)
+        menuInflater.inflate(R.menu.bottom_menu, menu)
         return true
     }
 }
